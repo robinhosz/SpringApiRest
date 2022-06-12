dae85d970a994e89d69898b88b87434e9ecb72fc
José Robson <joserobsonsiqueira23@hotmail.com>
2022-06-12 00:45:07 -0300

    PostMapping and JsonIgnoreProperty para o DTO

--- src/main/java/br/com/foursys/api/service/UserService.java
+++ src/main/java/br/com/foursys/api/service/UserService.java
@@ -2,10 +2,12 @@ package br.com.foursys.api.service;
 
 import java.util.List;
 
+import br.com.foursys.api.dto.UserDTO;
 import br.com.foursys.api.model.User;
 
 public interface UserService {
 
 	List<User> findAll();
 	User findById(Integer id);
+	User create(UserDTO obj);
 }
--- src/main/java/br/com/foursys/api/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/foursys/api/service/impl/UserServiceImpl.java
@@ -3,20 +3,25 @@ package br.com.foursys.api.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.foursys.api.dto.UserDTO;
 import br.com.foursys.api.model.User;
 import br.com.foursys.api.repository.UserRepository;
 import br.com.foursys.api.service.UserService;
 import br.com.foursys.api.service.exceptions.ObjectNotFoundException;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private ModelMapper mapper;
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	@Override
 	public User findById(Integer id) {
 		Optional<User> obj = repository.findById(id);
@@ -24,10 +29,13 @@ public class UserServiceImpl implements UserService{
 	}
 
 	public List<User> findAll() {
-		
+
 		return repository.findAll();
 	}
 
-	
-	
+	@Override
+	public User create(UserDTO obj) {
+		return repository.save(mapper.map(obj, User.class));
+	}
+
 }
