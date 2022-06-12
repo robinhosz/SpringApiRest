e9b028d8941aa98bcdbf6b7e438327485fb1687d
José Robson <joserobsonsiqueira23@hotmail.com>
2022-06-12 20:22:06 -0300

    Trocando de ide para facilitação de testes

--- src/main/java/br/com/foursys/api/repository/UserRepository.java
+++ src/main/java/br/com/foursys/api/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package br.com.foursys.api.repository;
 
-import java.util.Optional;
-
+import br.com.foursys.api.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.foursys.api.model.User;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer>{
 
 	Optional<User> findByEmail(String email);
+	Optional<User> findByName(String name);
 
 }
--- src/main/java/br/com/foursys/api/service/impl/UserServiceImpl.java
+++ src/main/java/br/com/foursys/api/service/impl/UserServiceImpl.java
@@ -1,18 +1,17 @@
 package br.com.foursys.api.service.impl;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import br.com.foursys.api.dto.UserDTO;
 import br.com.foursys.api.model.User;
 import br.com.foursys.api.repository.UserRepository;
 import br.com.foursys.api.service.UserService;
 import br.com.foursys.api.service.exceptions.DataIntegratyViolationException;
 import br.com.foursys.api.service.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
