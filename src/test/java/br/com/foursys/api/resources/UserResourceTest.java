970f41d995c207a56320be48d6f6e26397a14814
José Robson <joserobsonsiqueira23@hotmail.com>
2022-06-13 00:23:05 -0300

    removendo bugs

--- src/test/java/br/com/foursys/api/resources/UserResourceTest.java
+++ src/test/java/br/com/foursys/api/resources/UserResourceTest.java
@@ -31,8 +31,8 @@ class UserResourceTest {
     public static final Integer ID = 1;
     public static final int INDEX = 0;
 
-    private User user;
-    private UserDTO userDTO;
+    private User user = new User();
+    private UserDTO userDTO = new UserDTO();
 
     @InjectMocks
     private UserResource resource;
