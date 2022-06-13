0a0017b7e6c86c914efa7e21af55a145754a0a0b
José Robson <joserobsonsiqueira23@hotmail.com>
2022-06-12 22:40:14 -0300

    classe de testes para user resources iniciada

---
+++ src/test/java/br/com/foursys/api/resources/UserResourceTest.java
@@ -0,0 +1,64 @@
+package br.com.foursys.api.resources;
+
+import br.com.foursys.api.dto.UserDTO;
+import br.com.foursys.api.model.User;
+import br.com.foursys.api.service.impl.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserResourceTest {
+
+    public static final String NAME = "Valdir";
+    public static final String EMAIL = "valdir@mail.com";
+    public static final String PASSWORD = "123";
+    public static final Integer ID = 1;
+
+    private User user;
+    private UserDTO userDTO;
+
+    @InjectMocks
+    private UserResource resource;
+
+    @Mock
+    private ModelMapper mapper;
+
+    @Mock
+    private UserServiceImpl service;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startUser();
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    private void startUser() {
+        user = new User(ID, NAME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NAME, EMAIL, PASSWORD);
+    }
+}
